7157d6c732a7e9207ff5c8a8f719a864ea39daad
Nancy Luong <nancyluong96@gmail.com>
2018-11-17 19:54:14 -0500

    add trip modal

--- src/Frontend/trip/join-trip.jsx
+++ src/Frontend/trip/join-trip.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import styles from "./create-trip.css";
 
 class JoinTrip extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      codeValue: ''
+    }
+  }
+
+  onChange(event) {
+    this.setState = ({
+      codeValue: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="form-container">
         <h3>Join Trip</h3>
-        <input className="join-input"/>
+        <form>
+          <input type="text" name="codeValue" onChange={this.onChange}/>
+        </form>
       </div>
     )
   }
