3d8a099e4318dc8f19ca964069d7e21e59e934fe
Cynthia Zhou <cyntzhou@gmail.com>
2018-11-19 14:42:20 -0500

    Login + Signup connect backend + UI

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import axios from "axios";
 import Trips from "../Frontend/trip/trips.jsx";
 import Login from "../Frontend/user/login.jsx";
 import Settings from "../Frontend/user/settings.jsx";
 import Trip from "../Frontend/trip/trip.jsx";
 import Navbar from "../Frontend/components/navbar.jsx";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
 
 const Home = () => (
   <div>
@@ -12,19 +14,105 @@ const Home = () => (
   </div>
 )
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      user: null
+    };
+  }
+
+  userHasAuthenticated = (user) => {
+    this.setState({
+      isAuthenticated: true,
+      user: user
+    });
+    console.log("User logged in");
+    console.log(user);
+  }
+
+  logout = () => {
+    axios.post('api/users/signout')
+      .then(() => {
+        this.setState({
+          isAuthenticated: false,
+          user: null
+        })
+        // eventBus.$emit('signout-success', true);
+      })
+  }
+
   render () {
+    const {
+      isAuthenticated,
+      user
+    } = this.state;
+
     return (
       <Router>
         <div className="App">
           <header className="App-header">
-            <Navbar/>
+            <Navbar
+              logout={this.logout}
+            />
           </header>
+
           <div>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/trips" component={Trips} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/settings" component={Settings} />
-            <Route path="/trips/:id" component={Trip}/>
+            <Route exact path="/login" 
+              render={(props) => <Login 
+                {...props}
+                userHasAuthenticated={this.userHasAuthenticated} 
+              />}
+            />
+
+            <Route exact path="/" 
+              render={(props) => (
+                isAuthenticated ? (
+                  <Trips 
+                    {...props}
+                    user={user} 
+                  />
+                ) : (
+                  <Redirect to="/login"/>
+                )
+              )}
+            />
+            <Route exact path="/trips" 
+              render={(props) => (
+                isAuthenticated ? (
+                  <Trips 
+                    {...props}
+                    user={user} 
+                  />
+                ) : (
+                  <Redirect to="/login"/>
+                )
+              )}
+            />
+            <Route exact path="/settings" 
+              render={(props) => (
+                isAuthenticated ? (
+                  <Settings 
+                    {...props}
+                    user={user} 
+                  />
+                ) : (
+                  <Redirect to="/login"/>
+                )
+              )}
+            />
+            <Route path="/trips/:id"
+              render={(props) => (
+                isAuthenticated ? (
+                <Trip 
+                  {...props}
+                  user={user} 
+                />
+                ) : (
+                  <Redirect to="/login"/>
+                )
+              )}
+            />
           </div>
         </div>
       </Router>
