284dd44ae3edfdbec88a5e7ea55251280103ecb6
Nancy Luong <nancyluong96@gmail.com>
2018-11-19 13:17:47 -0500

    connecting trip UI to backend

--- src/Frontend/trip/trip.jsx
+++ src/Frontend/trip/trip.jsx
@@ -4,13 +4,15 @@ import Activities from "../activity/activities.jsx";
 import Itinerary from "../itinerary/itinerary.jsx";
 import CreateActivityModal from "../activity/create-activity-modal.jsx";
 import EditActivityModal from "../activity/edit-activity-modal.jsx";
+import EditTripModal from "./edit-trip-modal.jsx";
 
 class Trip extends React.Component {
   constructor() {
     super();
     this.state = {
       showCreateActivity: false,
-      showEditActivity: false
+      showEditActivity: false,
+      showEditTrip: false
     }
   }
 
@@ -22,12 +24,20 @@ class Trip extends React.Component {
     this.setState({showEditActivity: true});
   }
 
+  showEditTripModal = () => {
+    this.setState({showEditTrip: true});
+  }
+
   hideCreateModal = () => {
     this.setState({showCreateActivity: false});
   }
 
   hideEditModal = () => {
     this.setState({showEditActivity: false});
+  }
+
+  hideEditTripModal = () => {
+    this.setState({showEditTrip: false});
   }
 
   render() {
@@ -38,11 +48,19 @@ class Trip extends React.Component {
       )
     } else if (this.state.showEditActivity) {
       return (
-        <EditActivityModal hideEditModal={this.hideEditModal}/>
+        <EditActivityModal hideEditModal={this.hideEditModal} tripId={tripId}/>
+      )
+    } else if (this.state.showEditTrip) {
+      return (
+        <EditTripModal hideModal={this.hideEditTripModal}/>
       )
     } else {
       return (
         <div className="trip-container">
+          <div className="edit-trip">
+            <p>Click to edit trip: </p>
+            <i onClick={this.showEditTripModal} className="fa fa-edit"/>
+          </div>
           <Activities 
             showCreateModal={this.showCreateModal}
             showEditModal={this.showEditModal}
