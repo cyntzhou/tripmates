af169b87e9a7fb62f72421a1a4f0a2808f056700
Janice Lee <janicelee1120@gmail.com>
2018-11-20 02:02:00 -0500

    Fix messed up merging in activities routes

--- routes/activities.js
+++ routes/activities.js
@@ -165,8 +165,6 @@ const router = express.Router();
   * @throws {401} - if user not logged in
   */
   router.post('/downvote', async (req, res) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
    if (req.session.name !== undefined) {
      let id = req.body.id;
      let userId = req.body.userId;
@@ -184,49 +182,11 @@ const router = express.Router();
        let downvote = await Activities.downvote(id, userId);
        res.status(200).json(downvote).end();
      }
-=======
-   let id = req.body.id;
-   let userId = req.body.userId;
-   let upvoters = await Activities.getUpvoters(id);
-   let downvoters = await Activities.getDownvoters(id);
-   if (downvoters.includes(id)) {
-     res.status(400).json({
-       error: `Activity already downvoted.`,
-     }).end();
-   }
-   else if (upvoters.includes(id)) {
-     let downvote = await Activities.removeUpvote(id, userId);
-     res.status(200).json(downvote).end();
->>>>>>> 8da7d6884c9ce5b8ad068c1d66d3ab5991c511d8
    } else {
      res.status(401).json({
        error: `Must be logged in to downvote activity.`,
      }).end();
    }
-=======
-    if (req.session.name !== undefined) {
-      let id = req.body.id;
-      let userId = req.body.userId;
-      let upvoters = await Activities.getUpvoters(id);
-      let downvoters = await Activities.getDownvoters(id);
-      if (downvoters.includes(id)) {
-        res.status(400).json({
-          error: `Activity already downvoted.`,
-        }).end();
-      }
-      else if (upvoters.includes(id)) {
-        let downvote = await Activities.removeUpvote(id, userId);
-        res.status(200).json(downvote).end();
-      } else {
-        let downvote = await Activities.downvote(id, userId);
-        res.status(200).json(downvote).end();
-      }
-    } else {
-      res.status(401).json({
-        error: `Must be logged in to downvote activity.`,
-      }).end();
-    }
->>>>>>> 6b50455199876b1e286d555ec646f879c55f0707
   });
 
 /**
