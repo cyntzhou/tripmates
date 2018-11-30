a015b8cba644721157bc71edd52fd9769bf22dec
Cynthia Zhou <cyntzhou@gmail.com>
2018-11-30 13:54:13 -0500

    use .env for db config

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const mysql = require('mysql');
 
 const config = {
-  host: 'sql.mit.edu',
-  user: 'jellee',
-  password: 'janrice',
-  database: 'jellee+tripmates'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 class Database {
