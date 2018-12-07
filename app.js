1e64ba4dce54dcf024b1449ba1ab0c72fb60899f
Cynthia Zhou <cyntzhou@gmail.com>
2018-12-07 15:48:11 -0500

    heroku

--- app.js
+++ app.js
@@ -45,4 +45,16 @@ if (process.env.NODE_ENV !== "test") {
   console.log(`Listening on ${port}`);
 }
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
+
 module.exports = app;
