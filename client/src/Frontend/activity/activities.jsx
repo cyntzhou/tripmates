e872090bf8d938c04563924793234e7bd5577232
Cynthia Zhou <cyntzhou@gmail.com>
2018-12-08 15:33:06 -0500

    Filter activities by category

--- client/src/Frontend/activity/activities.jsx
+++ client/src/Frontend/activity/activities.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { uniq } from "lodash";
 import styles from "./activities.css";
 import ActivityItem from "./activity-item.jsx";
 import AddButton from "../components/add-button.jsx";
@@ -8,9 +9,10 @@ import AddButton from "../components/add-button.jsx";
 class Activities extends React.Component {
   constructor() {
     super()
-    // this.state = {
-    //   activitiesList: []
-    // }
+    this.state = {
+      showCategoriesSelection: false,
+      checkedCategories: []
+    }
   }
 
   componentDidMount() {
@@ -23,13 +25,76 @@ class Activities extends React.Component {
     // })
   }
 
+  toggleCategoriesSelection = () => {
+    this.setState({
+      showCategoriesSelection: !this.state.showCategoriesSelection
+    })
+  }
+
+  handleSelectCategory = (event) => {
+    const { checkedCategories } = this.state;
+    const category = event.target.value;
+    const categoryIndex = checkedCategories.indexOf(category);
+    if (categoryIndex > -1) {
+      // remove from list
+      this.setState({
+        checkedCategories: [...checkedCategories.slice(0,categoryIndex), ...checkedCategories.slice(categoryIndex+1)]
+      });
+    } else {
+      checkedCategories.push(category);
+    }
+  }
+
   render() {
+    const {
+      showCategoriesSelection,
+      checkedCategories
+    } = this.state;
+
     const {
       showEditModal,
       showCreateModal,
       toggleCreateEventModal,
       activitiesList
-    } = this.props
+    } = this.props;
+
+    const categories = uniq(activitiesList.filter((act) => act.category).map((act) => act.category));
+    // Sort by alphabetical order
+    categories.sort((word1, word2) => { 
+      if (word2.toLowerCase() > word1.toLowerCase()) {
+        return -1;
+      } else if (word1.toLowerCase() > word2.toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    });
+
+    const displayedActivitiesList = checkedCategories.length > 0 ? 
+      activitiesList.filter((activity) => {
+        return checkedCategories.indexOf(activity.category) > -1;
+      }) :
+      activitiesList;
+
+    const categoriesSelection = (
+      <div>
+        {categories.map((category, i) => {
+          const checked = checkedCategories.indexOf(category) !== -1;
+          return (
+            <div key={i}>
+              <input 
+                type="checkbox" 
+                value={category} 
+                onChange={this.handleSelectCategory}
+                checked={checked}
+              />
+              {category}
+              <br/>
+            </div>
+          )
+        })}
+      </div>
+    )
+
     return (
       <div className="activities-container">
         <div className="activity-header">
@@ -39,7 +104,21 @@ class Activities extends React.Component {
             onButtonClick={showCreateModal}
           />
         </div>
-          {activitiesList.map(function(act, index){
+        <div className="activities-filter-category" onClick={this.toggleCategoriesSelection}>
+          Filter by Category 
+          {!showCategoriesSelection &&
+            <i className="fas fa-caret-down"></i>
+          }
+          {showCategoriesSelection &&
+            <i className="fas fa-caret-up"></i>
+          }
+        </div>
+
+        {showCategoriesSelection &&
+          categoriesSelection
+        }
+        <div className="activities-list">
+          {displayedActivitiesList.map(function(act, index){
             return (<ActivityItem 
               key={index}
               showEditModal={showEditModal} 
@@ -47,6 +126,7 @@ class Activities extends React.Component {
               toggleCreateEventModal={toggleCreateEventModal}
             />)
           })}
+        </div>
       </div>
     )
   }
