070b49af9fa88b2e61ba7a058dcfb12bf4b4d463
Cynthia Zhou <cyntzhou@gmail.com>
2019-01-07 11:59:18 -0500

    center map based on markers (but zoom isn't set dynamically; hard-coded to 13 for now)

--- client/src/Frontend/trip/trip-map.jsx
+++ client/src/Frontend/trip/trip-map.jsx
@@ -10,8 +10,6 @@ class TripMap extends React.Component {
     super(props)
     this.state = {
       tripId: props.tripId,
-      center: [42.3601, -71.0589],
-      zoom: 13,
       activities: [],
       positions: [],
     }
@@ -36,9 +34,10 @@ class TripMap extends React.Component {
 
   getPositions = () => {
     const provider = new OpenStreetMapProvider();
-    const actWithPos = []
+    const actWithPos = [];
     this.state.activities.forEach(act => {
-      provider.search({ query: act.address }).then(res => {
+      if (act.address) {
+        provider.search({ query: act.address }).then(res => {
           const pos = [parseFloat(res[0].y), parseFloat(res[0].x)]
           actWithPos.push({
             activityName: act.name, 
@@ -49,20 +48,38 @@ class TripMap extends React.Component {
           this.setState({
             positions: actWithPos
           })
-      }).catch(err => console.log(err));
+        }).catch(err => console.log(err));
+      }
     })
   }
 
   render() {
     const {
-      positions,
+      positions
     } = this.state
     const {
       toggleMap
     } = this.props
+    const latLons = positions.map((place) => place.pos);
+    const defaultCenter = [42.3601, -71.0589];
+    const center = positions.length > 0 ? 
+      latLons
+        .reduce((sum, pos) => {
+          return [sum[0] + pos[0], sum[1] + pos[1]];
+        })
+        .map((num) => num / positions.length) :
+      defaultCenter;
+    const defaultZoom = 11;
+    const lats = latLons.map((pos) => pos[0]);
+    const lons = latLons.map((pos) => pos[1]);
+    // const zoom = positions.length > 0 ? 
+    //   Math.max(Math.max(...lats) - Math.min(...lats), Math.max(...lons) - Math.min(...lons)) * 70 :
+    //   defaultZoom;
+    // console.log(zoom);
+    const zoom = defaultZoom;
     return (
       <div id="trip-map">
-        <Map center={this.state.center} zoom={this.state.zoom}>
+        <Map center={center} zoom={zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
