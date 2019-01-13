4a3684a1437bc94c1761b63ec161df6cdd2d014a
Cynthia Zhou <cyntzhou@gmail.com>
2019-01-13 17:22:41 -0500

    Moving all files and history from MIT repo to public repo

--- client/src/Frontend/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(date) {
-  return date.getFullYear() + '-' + 
-        ('0' + (date.getMonth()+1)).slice(-2) + '-' +
-        ('0' + date.getDate()).slice(-2) + 'T' + 
-        ('0' + date.getHours()).slice(-2) + ':' + 
-        ('0' + date.getMinutes()).slice(-2);
-}
